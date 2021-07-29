8e1b047a4238d709d85cf6895a2d7eb33db93413
jhkang1517 <jhkang1517@gmail.com>
2021-07-29 23:27:35 +0900

    - [1차] 다트 게임
    W05 5nd Problem by Programmers

--- 2021-W05/dart-result.py
+++ 2021-W05/dart-result.py
@@ -1,9 +1,46 @@
 import re
 
+def is_int(string):
+    result = ''
+    for s in string:
+        try:
+            if type(int(s)) == int:
+                result += s
+            else:
+                pass
+        except:
+            pass
+    return int(result)
+
 def solution(dartResult=''):
-    answer = 0
-    print(re.findall('[0-9]{1,2}[a-zA-Z#*]{1,2}', dartResult))
+    fst = [0, 0, 0]
+    flag = 0
+    score_list = re.findall('[0-9]{1,2}[a-zA-Z#*]{1,2}', dartResult)
 
+    for s in score_list:
+        num = is_int(s)
+
+        if 'S' in s:
+            fst[flag] += num ** 1
+
+        elif 'D' in s:
+            fst[flag] += num ** 2
+
+        elif 'T' in s:
+            fst[flag] += num ** 3
+
+        if '*' in s:
+            if flag == 0:
+                fst[flag] *= 2
+
+            else:
+                fst[flag] *= 2
+                fst[flag-1] *= 2
+
+        if '#' in s:
+            fst[flag] *= -1
+
+        flag += 1
+
+    answer = sum(fst)
     return answer
-
-solution('1D2S#10S')
