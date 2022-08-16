018eb1861b2646121938ede066123c1c841fb013
jhkang1517 <jhkang1517@gmail.com>
2022-08-16 23:28:51 +0900

    baekjoon : 11286
    Signed-off-by: jhkang1517 <jhkang1517@gmail.com>

---
+++ baekjoon/11286.py
@@ -0,0 +1,13 @@
+import sys
+import heapq
+
+pq = []
+for _ in range(int(sys.stdin.readline())):
+    x = int(sys.stdin.readline())
+    if x != 0:
+            heapq.heappush(pq, (abs(x), x))
+    else:
+        if not pq:
+            print(0)
+        else:
+            print(heapq.heappop(pq)[1])
