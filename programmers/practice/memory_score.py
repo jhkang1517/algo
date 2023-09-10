19cddbd33fb967c38b088518c52271d7e2d7e745
jhkang1517 <jhkang1517@gmail.com>
2023-09-10 09:55:09 +0900

    추억 점수 zip함수 사용 및 try문 제거

--- programmers/practice/memory_score.py
+++ programmers/practice/memory_score.py
@@ -1,17 +1,11 @@
 def solution(name, yearning, photo):
-    answer = []
-    dct = {n: yearning[idx] for idx, n in enumerate(name)}
+    answer = list()
+    dct = dict(zip(name, yearning))
 
     for p in photo:
         cnt = 0
         for h in p:
-            try:
+            if h in dct:
                 cnt += dct[h]
-            except KeyError:
-                pass
         answer.append(cnt)
     return answer
-
-
-if __name__ == '__main__':
-    solution(["may", "kein", "kain", "radi"], [5, 10, 1, 3], [["may", "kein", "kain", "radi"], ["may", "kein", "brin", "deny"], ["kon", "kain", "may", "coni"]])
