d7219d73a8e7c874649a28a74d57729c22489e83
jhkang1517 <jhkang1517@gmail.com>
2023-09-10 10:50:36 +0900

    페인트 덧칠하기 : 시간 복잡도 개선

--- programmers/practice/apply_paint.py
+++ programmers/practice/apply_paint.py
@@ -1,16 +1,9 @@
 def solution(n, m, section):
-    answer = 0
-    painted = dict()
-
-    while section:
-        end = section.pop()
-
-        if end in painted:
-            continue
-        else:
-            answer += 1
-
-        for x in range(m):
-            painted[end - x] = True
+    answer = 1 # 무조건 한번은 칠하고 시작함
+    prev = section[0]  # 첫 번째로 페인트 칠을 시작할 지점
+    for sec in section:
+        if sec - prev >= m:  # 칠해야 할 구역에서 페인트 칠을 시도하였을 때, 롤러 길이보다 긴 경우,
+            prev = sec # 페인트 칠 시작 지점을 현재 구역으로 바꾸고,
+            answer += 1 # 페인트 칠 수행
 
     return answer
